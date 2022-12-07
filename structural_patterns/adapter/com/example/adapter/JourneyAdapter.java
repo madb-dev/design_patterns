bfd2b9561b8f12aba53a1bfdaf000fa6787c5acc
Miguel <migueldelbas@gmail.com>
2022-12-07 17:21:19 +0100

    Comments in adapter

--- structural_patterns/adapter/com/example/adapter/JourneyAdapter.java
+++ structural_patterns/adapter/com/example/adapter/JourneyAdapter.java
@@ -1,5 +1,11 @@
 package com.example.adapter;
 
 public interface JourneyAdapter {
+    /**
+     * Method to Adapt distance
+     *
+     * @return distance in KM
+     */
     double getDistance();
+
 }
